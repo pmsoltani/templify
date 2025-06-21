4dbd1a3fa8e86dc737f24eddfc5e2fa5182a5522
Pooria Soltani <pooria.ms@gmail.com>
2025-06-22 00:46:29 +0100

    feat: Update users table schema to make api_key and confirmation_token fields unique

--- scripts/dbSetup.js
+++ scripts/dbSetup.js
@@ -11,9 +11,9 @@ const createTableQuery = `
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
-    api_key VARCHAR(64) NOT NULL UNIQUE,
+    api_key VARCHAR(64) UNIQUE,
     is_confirmed BOOLEAN NOT NULL DEFAULT FALSE,
-    confirmation_token VARCHAR(64) DEFAULT NULL,
+    confirmation_token VARCHAR(64) UNIQUE,
     usage_count INTEGER NOT NULL DEFAULT 0,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
