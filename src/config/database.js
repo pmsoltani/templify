7e726219d9c1cf17fccf46441139f379817d782d
Pooria Soltani <pooria.ms@gmail.com>
2025-06-27 00:46:42 +0100

    refactor: remove SSL configuration from database connection, as it is managed by env var.

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,5 @@
 import { Pool } from "pg";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // For Render's free tier PostgreSQL
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 export default pool;
