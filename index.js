edaca9b6c36b2134b9248d4531656883810197c8
Pooria Soltani <pooria.ms@gmail.com>
2025-06-27 10:06:22 +0100

    refactor: restructure server initialization for improved lifespan management.

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 import express from "express";
 import apiRoutes from "./src/routes/api.js";
+import db from "./src/config/database.js";
+import { initializeBrowser, closeBrowserInstance } from "./src/config/puppeteer.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // Middleware to parse JSON request bodies
+const runServer = async () => {
+  try {
+    console.log("Starting server...");
+    await initializeBrowser();
+    await db.query("SELECT 1");
+    console.log("Database connection verified.");
 
-app.use("/api", apiRoutes);
-app.get("/", (req, res) => res.json({ message: "Welcome to Templify!" }));
+    app.use(express.json()); // Middleware to parse JSON request bodies
+    app.use("/api", apiRoutes);
+    app.get("/", (req, res) => res.json({ message: "Welcome to Templify!" }));
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+    const server = app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+
+    const gracefulShutdown = async () => {
+      console.log("Received shutdown signal, closing resources...");
+      await closeBrowserInstance();
+      await db.end();
+      console.log("Database pool closed.");
+      server.close(() => {
+        console.log("HTTP server closed. Exiting process.");
+        process.exit(0);
+      });
+    };
+
+    process.on("SIGTERM", gracefulShutdown); // Ctrl+C in the terminal (dev)
+    process.on("SIGINT", gracefulShutdown); // Graceful shutdown
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1); // Exit with an error code
+  }
+};
+
+runServer();
