6d6ee9be95b751eaf62cc806a5881d260577b772
Pooria Soltani <pooria.ms@gmail.com>
2025-06-27 09:23:31 +0100

    refactor: moved generatePdf function to the new pdfService module.

--- src/services/templateService.js
+++ src/services/templateService.js
@@ -1,10 +1,5 @@
 import fs from "fs";
-import path from "path";
-import { pathToFileURL } from "url";
-import mustache from "mustache";
-import { getBrowserInstance } from "../config/puppeteer.js";
 import * as fileService from "./fileService.js";
-import * as pdfRepo from "../repositories/pdfRepository.js";
 import * as templateRepo from "../repositories/templateRepository.js";
 
 const getAllByUserId = async (userId) => {
@@ -17,48 +12,6 @@ const create = async (userId, templateName, htmlEntrypoint, tempZipPath) => {
   await fileService.unzipAndUpload(tempZipPath, `userFiles/${userId}/${templateDb.id}`);
   fs.unlinkSync(tempZipPath); // Clean up the temp file
   return templateDb;
-};
-
-const generatePdf = async (userId, templateId, jsonData) => {
-  let tempDir = null;
-  let page = null;
-
-  try {
-    // Authenticate and fetch template record
-    const templateDb = await templateRepo.getByIdAndUserId(templateId, userId);
-    if (!templateDb) throw new Error("Template not found.");
-
-    // Fetch template files from storage
-    const bucketPath = `userFiles/${userId}/${templateId}/`;
-    tempDir = await fileService.downloadTemplate(bucketPath);
-
-    // Inject JSON data into HTML template
-    const htmlPath = path.join(tempDir, templateDb.html_entrypoint);
-    const htmlContent = fs.readFileSync(htmlPath, "utf-8");
-    const finalHtml = mustache.render(htmlContent, jsonData);
-
-    // Generate PDF with Puppeteer
-    const browser = await getBrowserInstance();
-    const page = await browser.newPage();
-
-    // Tell puppeteer to treat the temp dir as the base for linked assets (CSS, images)
-    const fileUrl = pathToFileURL(htmlPath).href;
-    await page.goto(fileUrl, { waitUntil: "networkidle0" });
-    await page.setContent(finalHtml, { waitUntil: "networkidle0" }); // Final HTML file
-    const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
-
-    // Upload PDF to storage and return the public URL
-    const storageObjectKey = await fileService.uploadPdf(userId, pdfBuffer);
-
-    // Log the usage
-    await pdfRepo.create(userId, templateId, storageObjectKey);
-
-    // Return a presigned URL for the PDF
-    return await fileService.getPresignedUrl(storageObjectKey);
-  } finally {
-    if (tempDir) fs.rmSync(tempDir, { recursive: true, force: true });
-    if (page) await page.close();
-  }
 };
 
 const remove = async (userId, templateId) => {
@@ -86,4 +39,4 @@ const update = async (userId, templateId, updateData, tempZipPath) => {
   );
 };
 
-export { getAllByUserId, create, generatePdf, remove, update };
+export { getAllByUserId, create, remove, update };
