e605839433f051ed18cd8e0d7482ff00cb154d4c
Pooria Soltani <pooria.ms@gmail.com>
2025-06-28 12:23:52 +0100

    feat: extend AppError constructor to include optional error details

--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -1,10 +1,16 @@
 class AppError extends Error {
-  constructor(message, statusCode) {
+  /**
+   * @param {string} message The error message.
+   * @param {number} statusCode The HTTP status code.
+   * @param {object|null} details Optional object with extra error details.
+   */
+  constructor(message, statusCode, details = null) {
     super(message);
 
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
     this.isOperational = true; // To distinguish from programming errors
+    if (details) this.details = details;
 
     Error.captureStackTrace(this, this.constructor);
   }
