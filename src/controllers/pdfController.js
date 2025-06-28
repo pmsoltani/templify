01f8f60d7d73ab5866b675b3819f3a78f198fd78
Pooria Soltani <pooria.ms@gmail.com>
2025-06-28 19:36:49 +0100

    feat: enhance PDF controller responses with success messages and correct variable usage

--- src/controllers/pdfController.js
+++ src/controllers/pdfController.js
@@ -5,17 +5,17 @@ import { publicPdfs } from "../schemas/pdfSchema.js";
 
 const getAll = async (req, res) => {
   const pdfsDb = await pdfRepo.getAllByUserId(req.user.userId);
-  res.json(publicPdfs.parse(pdfsDb));
+  res.json({ message: "PDFs retrieved successfully.", data: publicPdfs.parse(pdfsDb) });
 };
 
 const getDownloadLink = async (req, res) => {
   const pdfId = req.params.id;
   const userId = req.user.userId;
 
-  const pdfRecord = await pdfRepo.getByIdAndUser(pdfId, userId);
-  if (!pdfRecord) throw new AppError("PDF record not found.", 404);
+  const pdfDb = await pdfRepo.getByIdAndUser(pdfId, userId);
+  if (!pdfDb) throw new AppError("PDF record not found.", 404);
 
-  const newUrl = await fileService.getPresignedUrl(pdfRecord.r2_object_key);
+  const newUrl = await fileService.getPresignedUrl(pdfDb.storage_object_key);
   res.json({ url: newUrl });
 };
 
