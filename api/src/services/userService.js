477fe305e9ca193d2078e833e4ab6fb4eb27dfef
Pooria Soltani <pooria.ms@gmail.com>
2025-07-03 17:55:02 +0100

    ✨ refactor: update user service methods to use publicId instead of userId

--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -5,12 +5,12 @@ import * as mailer from "./mailerService.js";
 import * as secretService from "./secretService.js";
 import AppError from "../utils/AppError.js";
 
-const updateEmail = async (userId, newEmail) => {
+const updateEmail = async (publicId, newEmail) => {
   const existingUser = await userRepo.getByEmailOrNewEmail(newEmail);
   if (existingUser) throw new AppError("Email already in use.", 409);
 
   const confirmationToken = secretService.generateSecureRandomToken();
-  await userRepo.update(userId, {
+  await userRepo.update(publicId, {
     new_email: newEmail,
     confirmation_token: confirmationToken,
     is_confirmed: false,
@@ -18,8 +18,8 @@ const updateEmail = async (userId, newEmail) => {
   await mailer.sendConfirmationEmail(newEmail, confirmationToken);
 };
 
-const updatePassword = async (userId, currentPassword, newPassword) => {
-  const userDb = await userRepo.getByPublicId(userId);
+const updatePassword = async (publicId, currentPassword, newPassword) => {
+  const userDb = await userRepo.getByPublicId(publicId);
   const isValid = await secretService.verifyPassword(
     currentPassword,
     userDb.password_hash
@@ -27,26 +27,27 @@ const updatePassword = async (userId, currentPassword, newPassword) => {
   if (!isValid) throw new AppError("Invalid current password.", 401);
 
   const newPasswordHash = await secretService.generatePasswordHash(newPassword);
-  return userRepo.update(userId, { password_hash: newPasswordHash });
+  return userRepo.update(publicId, { password_hash: newPasswordHash });
 };
 
-const regenerateApiKey = async (userId) => {
+const regenerateApiKey = async (publicId) => {
   const newApiKey = secretService.generateSecureRandomToken();
-  return userRepo.update(userId, { api_key: newApiKey });
+  return userRepo.update(publicId, { api_key: newApiKey });
 };
 
-const remove = async (userId, password) => {
-  const userDb = await userRepo.getByPublicId(userId);
+const remove = async (publicId, password) => {
+  const userDb = await userRepo.getByPublicId(publicId);
   const isValid = await secretService.verifyPassword(password, userDb.password_hash);
   if (!isValid) throw new AppError("Invalid password.", 401);
 
   // Remove all user's templates
-  const templates = await templateRepo.getAllByUserId(userId);
-  for (const template of templates) {
-    const bucketPath = `userFiles/${userId}/${template.id}/`;
+  const templatesDb = await templateRepo.getAllByUserPublicId(publicId);
+  for (const template of templatesDb) {
+    const bucketPath = `userFiles/${publicId}/${template.id}/`;
     await fileService.removeTemplate(bucketPath);
   }
-  return userRepo.remove(userId);
+
+  return userRepo.remove(publicId); // Postgres will cascade delete the related records
 };
 
 export { updateEmail, updatePassword, regenerateApiKey, remove };
