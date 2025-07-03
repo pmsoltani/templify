8f2377e3ecd94f8437b0e8928e77de81487c723e
Pooria Soltani <pooria.ms@gmail.com>
2025-07-03 12:11:01 +0100

    ✨ fix: rename generateId to generatePublicId

--- api/src/services/secretService.js
+++ api/src/services/secretService.js
@@ -18,23 +18,6 @@ const generateAuthToken = (user) => {
   return jwt.sign(payload, secret, options);
 };
 
-/**
- * Generates a unique public ID for a given entity type by prefixing a nanoid
- * with the entity's prefix.
- * @param {string} entityType - The type of entity for which to generate the ID.
- * @returns {string} The generated unique ID.
- * @throws {AppError} Throws an error if the entity type is unknown or has no prefix.
- */
-const generateId = (entityType) => {
-  const prefix = ENTITIES[entityType].prefix;
-  if (!prefix) {
-    const error = new AppError(`Unknown entity type: ${entityType}`, 500);
-    error.isOperational = false;
-    throw error;
-  }
-  return `${prefix}${nanoid()}`;
-};
-
 /** * Generates a password hash using bcrypt.
  * @param {string} password The plaintext password to hash.
  * @returns {Promise<string>} The hashed password.
@@ -54,6 +37,23 @@ const generateSecureRandomToken = (bytes = 32) => {
   return crypto.randomBytes(bytes).toString("hex");
 };
 
+/**
+ * Generates a unique public ID for a given entity type by prefixing a nanoid
+ * with the entity's prefix.
+ * @param {string} entityType - The type of entity for which to generate the ID.
+ * @returns {string} The generated unique ID.
+ * @throws {AppError} Throws an error if the entity type is unknown or has no prefix.
+ */
+const generatePublicId = (entityType) => {
+  const prefix = ENTITIES[entityType].prefix;
+  if (!prefix) {
+    const error = new AppError(`Unknown entity type: ${entityType}`, 500);
+    error.isOperational = false;
+    throw error;
+  }
+  return `${prefix}${nanoid()}`;
+};
+
 /** * Compares a plaintext password with a hashed password.
  * @param {string} plainPassword The plaintext password to compare.
  * @param {string} hashedPassword The hashed password to compare against.
@@ -66,6 +66,7 @@ const verifyPassword = async (plainPassword, hashedPassword) => {
 export {
   generateAuthToken,
   generatePasswordHash,
+  generatePublicId,
   generateSecureRandomToken,
   verifyPassword,
 };
