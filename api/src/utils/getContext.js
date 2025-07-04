023b70a45a13d30136234fedf11a8439a54ff849
Pooria Soltani <pooria.ms@gmail.com>
2025-07-04 15:48:32 +0100

    ✨ feat: enhance getContext function to include user information in the context object

--- api/src/utils/getContext.js
+++ api/src/utils/getContext.js
@@ -1,7 +1,15 @@
+/**
+ * Extracts context information from the HTTP request object.
+ *
+ * @param {Object} req - The HTTP request object.
+ * @returns {Object} The context obj containing ipAddress, userAgent, & optionally user.
+ */
 const getContext = (req) => {
   const ipAddress = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   const userAgent = req.headers["user-agent"] || "unknown";
-  return { ipAddress, userAgent };
+  const context = { ipAddress, userAgent };
+  if (req.user) context.user = req.user;
+  return context;
 };
 
 export default getContext;
