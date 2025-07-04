84e164509d0253eff89bea0c5a2f521772bfec6c
Pooria Soltani <pooria.ms@gmail.com>
2025-07-04 15:51:36 +0100

    ✨ feat: refactor user service methods to use context and improve error handling

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -1,11 +1,9 @@
-import * as userRepo from "../repositories/userRepository.js";
-import * as userService from "../services/userService.js";
-import AppError from "../utils/AppError.js";
 import { publicUser } from "../schemas/userSchema.js";
+import UserService from "../services/UserService.js";
+import getContext from "../utils/getContext.js";
 
 const get = async (req, res) => {
-  const userDb = await userRepo.getByPublicId(req.user.id);
-  if (!userDb) throw new AppError("User not found.", 404);
+  const userDb = await new UserService(getContext(req)).get();
 
   res.json({
     message: "User retrieved successfully!",
@@ -14,22 +12,18 @@ const get = async (req, res) => {
 };
 
 const updateEmail = async (req, res) => {
-  const newEmail = req.body.email;
-  if (req.user.email === newEmail) {
-    throw new AppError("Cannot use the current email.", 400);
-  }
-  await userService.updateEmail(req.user.id, newEmail);
+  await new UserService(getContext(req)).updateEmail(req.body.email);
   res.json({ message: "Check your new email for the confirmation link." });
 };
 
 const updatePassword = async (req, res) => {
   const { currentPassword, newPassword } = req.body;
-  await userService.updatePassword(req.user.id, currentPassword, newPassword);
+  await new UserService(getContext(req)).updatePassword(currentPassword, newPassword);
   res.json({ message: "Password updated successfully." });
 };
 
 const regenerateApiKey = async (req, res) => {
-  const userDb = await userService.regenerateApiKey(req.user.id);
+  const userDb = await new UserService(getContext(req)).regenerateApiKey();
   res.json({
     message: "API Key regenerated successfully!",
     data: { user: publicUser.parse(userDb) },
@@ -37,8 +31,7 @@ const regenerateApiKey = async (req, res) => {
 };
 
 const remove = async (req, res) => {
-  console.log("Removing user:", req.user);
-  await userService.remove(req.user.id, req.body.password);
+  await new UserService(getContext(req)).remove(req.body.password);
   res.status(204).send();
 };
 
--- api/src/services/UserService.js
+++ api/src/services/UserService.js
@@ -1,3 +1,4 @@
+import * as eventRepo from "../repositories/eventRepository.js";
 import * as pdfRepo from "../repositories/pdfRepository.js";
 import * as templateRepo from "../repositories/templateRepository.js";
 import * as userRepo from "../repositories/userRepository.js";
@@ -5,55 +6,85 @@ import * as fileService from "./fileService.js";
 import * as mailer from "./mailerService.js";
 import * as secretService from "./secretService.js";
 import AppError from "../utils/AppError.js";
+import { log } from "./eventService.js";
 
-const updateEmail = async (publicId, newEmail) => {
-  const existingUser = await userRepo.getByEmailOrNewEmail(newEmail);
-  if (existingUser) throw new AppError("Email already in use.", 409);
-
-  const confirmationToken = secretService.generateSecureRandomToken();
-  await userRepo.update(publicId, {
-    new_email: newEmail,
-    confirmation_token: confirmationToken,
-    is_confirmed: false,
-  });
-  await mailer.sendConfirmationEmail(newEmail, confirmationToken);
-};
-
-const updatePassword = async (publicId, currentPassword, newPassword) => {
-  const userDb = await userRepo.getByPublicId(publicId);
-  const isValid = await secretService.verifyPassword(
-    currentPassword,
-    userDb.password_hash
-  );
-  if (!isValid) throw new AppError("Invalid current password.", 401);
-
-  const newPasswordHash = await secretService.generatePasswordHash(newPassword);
-  return userRepo.update(publicId, { password_hash: newPasswordHash });
-};
-
-const regenerateApiKey = async (publicId) => {
-  const newApiKey = secretService.generateSecureRandomToken();
-  return userRepo.update(publicId, { api_key: newApiKey });
-};
-
-const remove = async (publicId, password) => {
-  const userDb = await userRepo.getByPublicId(publicId);
-  const isValid = await secretService.verifyPassword(password, userDb.password_hash);
-  if (!isValid) throw new AppError("Invalid password.", 401);
-
-  // Remove all user's PDFs
-  const pdfsDb = await pdfRepo.getAllByUserPublicId(publicId);
-  const pdfKeys = pdfsDb.map((pdf) => pdf.storage_object_key);
-  await fileService.removePdfs(pdfKeys);
-
-  // Remove all user's templates
-  const templatesDb = await templateRepo.getAllByUserPublicId(publicId);
-  for (const template of templatesDb) {
-    const bucketPath = `userFiles/${publicId}/templates/${template.public_id}/`;
-    await fileService.removeTemplate(bucketPath);
+export default class AuthService {
+  constructor(context = {}) {
+    this.context = context;
   }
 
-  return userRepo.remove(publicId); // Postgres will cascade delete the related records
-};
+  async get() {
+    const publicId = this.context.user.id;
+    const logData = { userPublicId: publicId, action: "USER_GET" };
+    const userDb = await userRepo.getByPublicId(publicId);
+    if (!userDb) throw new AppError("User not found.", 404, { logData });
+    await log(logData.userPublicId, logData.action, "SUCCESS", this.context);
+    return userDb;
+  }
 
-export { updateEmail, updatePassword, regenerateApiKey, remove };
+  async updateEmail(newEmail) {
+    const logData = { userPublicId: this.context.user.id, action: "USER_EMAIL_UPDATE" };
+    if (this.context.user.email === newEmail) {
+      throw new AppError("Cannot use the current email.", 400, { logData });
+    }
+    const existingUser = await userRepo.getByEmailOrNewEmail(newEmail);
+    if (existingUser) throw new AppError("Email already in use.", 409, { logData });
+
+    const confirmationToken = secretService.generateSecureRandomToken();
+    await userRepo.update(this.context.user.id, {
+      new_email: newEmail,
+      confirmation_token: confirmationToken,
+      is_confirmed: false,
+    });
+    await mailer.sendConfirmationEmail(newEmail, confirmationToken);
+    await log(logData.userPublicId, logData.action, "SUCCESS", this.context);
+  }
+
+  async updatePassword(currentPassword, newPassword) {
+    const publicId = this.context.user.id;
+    const logData = { userPublicId: publicId, action: "USER_PASSWORD_UPDATE" };
+    const userDb = await userRepo.getByPublicId(publicId);
+    const isValid = await secretService.verifyPassword(
+      currentPassword,
+      userDb.password_hash
+    );
+    if (!isValid) throw new AppError("Invalid current password.", 401, { logData });
+
+    const newPasswordHash = await secretService.generatePasswordHash(newPassword);
+    await log(logData.userPublicId, logData.action, "SUCCESS", this.context);
+    return userRepo.update(publicId, { password_hash: newPasswordHash });
+  }
+
+  async regenerateApiKey() {
+    const publicId = this.context.user.id;
+    const newApiKey = secretService.generateSecureRandomToken();
+    await log(publicId, "USER_APIKEY_GENERATE", "SUCCESS", this.context);
+    return userRepo.update(publicId, { api_key: newApiKey });
+  }
+
+  async remove(password) {
+    const publicId = this.context.user.id;
+    const logData = { userPublicId: publicId, action: "USER_ACCOUNT_REMOVE" };
+    const userDb = await userRepo.getByPublicId(publicId);
+    const isValid = await secretService.verifyPassword(password, userDb.password_hash);
+    if (!isValid) throw new AppError("Invalid password.", 401, { logData });
+
+    // Anonymize user data
+    await eventRepo.anonymizeByUserId(userDb.id);
+
+    // Remove all user's PDFs
+    const pdfsDb = await pdfRepo.getAllByUserPublicId(publicId);
+    const pdfKeys = pdfsDb.map((pdf) => pdf.storage_object_key);
+    await fileService.removePdfs(pdfKeys);
+
+    // Remove all user's templates
+    const templatesDb = await templateRepo.getAllByUserPublicId(publicId);
+    for (const template of templatesDb) {
+      const bucketPath = `userFiles/${publicId}/templates/${template.public_id}/`;
+      await fileService.removeTemplate(bucketPath);
+    }
+
+    await userRepo.remove(publicId); // Postgres will cascade delete the related records
+    await log(logData.userPublicId, logData.action, "SUCCESS"); // Don't add context
+  }
+}
