dfebe93a71819498f31d64e93c25a75656f63864
Pooria Soltani <pooria.ms@gmail.com>
2025-07-04 18:51:12 +0100

    ✨ feat: refactor template controller methods to use TemplateService and improve error handling

--- api/src/controllers/templateController.js
+++ api/src/controllers/templateController.js
@@ -1,41 +1,27 @@
-import fs from "fs";
-import * as templateService from "../services/templateService.js";
-import * as pdfService from "../services/pdfService.js";
-import AppError from "../utils/AppError.js";
 import { publicTemplate, publicTemplates } from "../schemas/templateSchema.js";
+import * as pdfService from "../services/pdfService.js";
+import TemplateService from "../services/TemplateService.js";
+import getContext from "../utils/getContext.js";
 
 const getAllByUserId = async (req, res) => {
-  const templatesDb = await templateService.getAllByUserPublicId(req.user.id);
+  const templatesDb = await new TemplateService(getContext(req)).getAllByUserPublicId();
   res.json({
     message: "Templates retrieved successfully!",
     data: { templates: publicTemplates.parse(templatesDb) },
   });
 };
 
-const create = async (req, res, next) => {
-  const userPublicId = req.user.id;
-  try {
-    const { name, htmlEntrypoint, description = null } = req.body;
-    if (!req.file) throw new AppError("Missing template file.", 400);
-
-    const templateDb = await templateService.create(
-      userPublicId,
-      name,
-      htmlEntrypoint || "template.html",
-      description,
-      req.file.path
-    );
-    templateDb.user_public_id = templateDb.user_public_id || userPublicId;
-    res.status(201).json({
-      message: "Template created successfully!",
-      data: { template: publicTemplate.parse(templateDb) },
-    });
-  } catch (err) {
-    try {
-      fs.unlinkSync(req.file.path); // Clean up the temp file
-    } catch (err) {}
-    next(err); // Pass error to the error handler
-  }
+const create = async (req, res) => {
+  const templateDb = await new TemplateService(getContext(req)).create(
+    req.body.name,
+    req.body.htmlEntrypoint,
+    req.body.description,
+    req.file?.path
+  );
+  res.status(201).json({
+    message: "Template created successfully!",
+    data: { template: publicTemplate.parse(templateDb) },
+  });
 };
 
 const generatePdf = async (req, res) => {
@@ -44,30 +30,20 @@ const generatePdf = async (req, res) => {
 };
 
 const remove = async (req, res) => {
-  await templateService.remove(req.user.id, req.params.id);
+  await new TemplateService(getContext(req)).remove(req.params.id);
   res.status(204).send();
 };
 
-const update = async (req, res, next) => {
-  const publicId = req.params.id;
-  const userPublicId = req.user.id;
-  try {
-    if (!req.file) throw new AppError("Missing template file.", 400);
-    const templateDb = await templateService.update(
-      userPublicId,
-      publicId,
-      req.body,
-      req.file.path
-    );
-    templateDb.user_public_id = templateDb.user_public_id || userPublicId;
-    res.json({
-      message: "Template update successful!",
-      data: { template: publicTemplate.parse(templateDb) },
-    });
-  } catch (err) {
-    if (req.file) fs.unlinkSync(req.file.path); // Clean up the temp file
-    next(err); // Pass error to the error handler
-  }
+const update = async (req, res) => {
+  const templateDb = await new TemplateService(getContext(req)).update(
+    req.params.id,
+    req.body,
+    req.file?.path
+  );
+  res.json({
+    message: "Template update successful!",
+    data: { template: publicTemplate.parse(templateDb) },
+  });
 };
 
 export { getAllByUserId, create, generatePdf, remove, update };
