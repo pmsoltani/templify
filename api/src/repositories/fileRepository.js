c86b1c84fd048490281190815b072178d1b1dfbc
Pooria Soltani <pooria.ms@gmail.com>
2025-07-05 16:03:02 +0100

    ✨ feat: add fileRepository with CRUD operations for file management

---
+++ api/src/repositories/fileRepository.js
@@ -0,0 +1,56 @@
+import db from "../config/database.js";
+
+const getByPublicId = async (publicId) => {
+  const res = await db.query("SELECT * FROM files WHERE public_id = $1;", [publicId]);
+  return res.rows[0];
+};
+
+const getAllByTemplatePublicId = async (templatePublicId) => {
+  const res = await db.query(
+    `
+    SELECT
+      f.*,
+      t.public_id AS template_public_id
+    FROM files f
+    JOIN templates t ON f.template_id = t.id
+    WHERE t.public_id = $1
+    ORDER BY f.name DESC;
+    `,
+    [templatePublicId]
+  );
+  return res.rows;
+};
+
+const create = async (publicId, templatePublicId, name, size, mime) => {
+  const res = await db.query(
+    `
+    INSERT INTO files (public_id, template_id, name, size, mime)
+    VALUES ($1, (SELECT id FROM templates WHERE public_id = $2), $3, $4, $5)
+    RETURNING *;
+    `,
+    [publicId, templatePublicId, name, size, mime]
+  );
+  return res.rows[0];
+};
+
+const remove = async (publicId) => {
+  const res = await db.query("DELETE FROM files WHERE public_id = $1 RETURNING *;", [
+    publicId,
+  ]);
+  return res.rows[0];
+};
+
+const update = async (publicId, name, size, mime) => {
+  const res = await db.query(
+    `
+    UPDATE files
+    SET name = $2, size = $3, mime = $4
+    WHERE public_id = $1
+    RETURNING *;
+    `,
+    [publicId, name, size, mime]
+  );
+  return res.rows[0];
+};
+
+export { create, getAllByTemplatePublicId, getByPublicId, remove, update };
