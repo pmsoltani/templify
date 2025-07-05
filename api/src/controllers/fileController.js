664a094382f495f52e75f1e135b62c44dc9df2e5
Pooria Soltani <pooria.ms@gmail.com>
2025-07-05 16:03:17 +0100

    ✨ feat: implement fileController with CRUD operations for file management

---
+++ api/src/controllers/fileController.js
@@ -0,0 +1,49 @@
+import { publicFile, publicFiles } from "../schemas/fileSchema.js";
+import FileService from "../services/FileService.js";
+import getContext from "../utils/getContext.js";
+
+const getAllByTemplateId = async (req, res) => {
+  const filesDb = await new FileService(getContext(req)).getAllByTemplatePublicId(
+    req.params.templateId
+  );
+  res.json({
+    message: "Files retrieved successfully!",
+    data: { files: publicFiles.parse(filesDb) },
+  });
+};
+
+const create = async (req, res) => {
+  console.log("------", req.params.templateId, "------");
+  const fileDb = await new FileService(getContext(req)).create(
+    req.params.templateId,
+    req.body.name || req.file?.originalname,
+    req.file?.path
+  );
+  res.status(201).json({
+    message: "File created successfully!",
+    data: { file: publicFile.parse(fileDb) },
+  });
+};
+
+const remove = async (req, res) => {
+  await new FileService(getContext(req)).remove(
+    req.params.fileId,
+    req.params.templateId
+  );
+  res.status(204).send();
+};
+
+const update = async (req, res) => {
+  const fileDb = await new FileService(getContext(req)).update(
+    req.params.fileId,
+    req.params.templateId,
+    req.body.name,
+    req.file?.path
+  );
+  res.json({
+    message: "File update successful!",
+    data: { file: publicFile.parse(fileDb) },
+  });
+};
+
+export { create, getAllByTemplateId, remove, update };
