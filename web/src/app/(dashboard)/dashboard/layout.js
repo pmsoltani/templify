1284834e10887ae35d91a315858575018b75b04f
Pooria Soltani <pooria.ms@gmail.com>
2025-07-09 13:08:16 +0100

    ✨ refactor: remove unused components and context for code cleanup

--- web/src/app/(dashboard)/dashboard/layout.js
+++
@@ -1,6 +0,0 @@
-import { DashboardProvider } from "./context/DashboardContext";
-
-export default function DashboardLayout({ children }) {
-  // This provider will make dashboard state available to the page and all its children.
-  return <DashboardProvider>{children}</DashboardProvider>;
-}
