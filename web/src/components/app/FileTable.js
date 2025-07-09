aecc7d78674e7245fc889f99f7e6780a84b8a656
Pooria Soltani <pooria.ms@gmail.com>
2025-07-09 23:42:23 +0100

    🐛 fix: update colspan in FileTable component to accommodate additional columns

--- web/src/components/app/FileTable.js
+++ web/src/components/app/FileTable.js
@@ -51,7 +51,7 @@ export default function FileTable() {
       <TableBody>
         {currentFiles.length === 0 ? (
           <TableRow>
-            <TableCell colSpan={4}>
+            <TableCell colSpan={6}>
               <Status
                 type="empty"
                 title="No files yet"
