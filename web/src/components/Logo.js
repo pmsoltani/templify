5c0f1f59154581a57a61f4cbc67aaf34dd880c91
Pooria Soltani <pooria.ms@gmail.com>
2025-07-09 13:02:26 +0100

    ✨ feat: add Logo component for branding display

---
+++ web/src/components/Logo.js
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <span className="flex items-center justify-center w-8 h-8 bg-blue-600 rounded-lg text-white font-bold text-sm">
+      T
+    </span>
+  );
+}
