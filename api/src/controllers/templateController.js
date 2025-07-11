c15999f97ce8e45c53d1bd44ad0f601cc022299b
Pooria Soltani <pooria.ms@gmail.com>
2025-07-11 22:03:27 +0100

    🐛 fix: remove unused create and update methods, and standardize response status codes in PDF generation and preview

--- api/src/controllers/templateController.js
+++ api/src/controllers/templateController.js
@@ -25,25 +25,12 @@ const createSlim = async (req, res) => {
   });
 };
 
-const create = async (req, res) => {
-  const templateDb = await new TemplateService(getContext(req)).create(
-    req.body.name,
-    req.body.htmlEntrypoint,
-    req.body.description,
-    req.file?.path
-  );
-  res.status(201).json({
-    message: "Template created successfully!",
-    data: { template: publicTemplate.parse(templateDb) },
-  });
-};
-
 const generatePdf = async (req, res) => {
   const pdfDb = await new PdfService(getContext(req)).generatePdf(
     req.params.templateId,
     req.body
   );
-  res.json({
+  res.status(201).json({
     message: "PDF generated successfully!",
     data: { pdf: publicPdf.parse(pdfDb) },
   });
@@ -55,24 +42,15 @@ const generatePdfPreview = async (req, res) => {
     req.body,
     true
   );
-  res.json({ message: "PDF preview generated successfully!", data: { tempUrl } });
+  res.status(201).json({
+    message: "PDF preview generated successfully!",
+    data: { tempUrl },
+  });
 };
 
 const remove = async (req, res) => {
   await new TemplateService(getContext(req)).remove(req.params.templateId);
   res.status(204).send();
-};
-
-const update = async (req, res) => {
-  const templateDb = await new TemplateService(getContext(req)).update(
-    req.params.templateId,
-    req.body,
-    req.file?.path
-  );
-  res.json({
-    message: "Template update successful!",
-    data: { template: publicTemplate.parse(templateDb) },
-  });
 };
 
 const updateInfo = async (req, res) => {
@@ -90,20 +68,18 @@ const getVariables = async (req, res) => {
   const variables = await new TemplateService(getContext(req)).getVariables(
     req.params.templateId
   );
-  res.status(200).json({
+  res.json({
     message: "Template variables retrieved successfully!",
     data: { variables },
   });
 };
 
 export {
-  create,
   createSlim,
   generatePdf,
   generatePdfPreview,
   getAllByUserId,
   getVariables,
   remove,
-  update,
   updateInfo,
 };
