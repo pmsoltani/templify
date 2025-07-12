7b093c007766668b58217a3d5fbf482cfe3daa54
Pooria Soltani <pooria.ms@gmail.com>
2025-07-12 20:33:25 +0100

    ✨ feat: enhance validation middleware to modify request using parsed schema results

--- api/src/middlewares/validate.js
+++ api/src/middlewares/validate.js
@@ -7,11 +7,16 @@ import AppError from "../utils/AppError.js";
  */
 const validate = (schema) => (req, res, next) => {
   try {
-    schema.parse({
+    const parsed = schema.parse({
       body: req.body,
       query: req.query,
       params: req.params,
     });
+
+    req.body = parsed.body;
+    Object.assign(req.query, parsed.query);
+    req.params = parsed.params;
+
     next();
   } catch (err) {
     if (err instanceof z.ZodError) {
