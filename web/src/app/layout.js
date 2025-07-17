4dc21ced0e8fea6be6b2199a8fcd00b456c6cbc6
Pooria Soltani <pooria.ms@gmail.com>
2025-07-17 13:51:20 +0100

    ✨ feat: add Toaster component for notifications in RootLayout

--- web/src/app/layout.js
+++ web/src/app/layout.js
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 export const metadata = {
@@ -53,6 +54,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="antialiased flex flex-col items-center min-h-screen bg-gray-50 dark:bg-gray-950">
         {children}
+        <Toaster position="top-right" richColors />
       </body>
     </html>
   );
