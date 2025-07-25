c1ab3594757548c02d1ec822a69b123e2d90ce78
Pooria Soltani <pooria.ms@gmail.com>
2025-07-25 13:00:35 +0100

    ✨ feat: refactor s3Client configuration to use STORAGE environment variables

--- api/src/config/s3Client.js
+++ api/src/config/s3Client.js
@@ -1,12 +1,19 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
+const BUCKETS = {
+  hub: process.env.STORAGE_BUCKET_HUB,
+  pdfs: process.env.STORAGE_BUCKET_PDFS,
+  previews: process.env.STORAGE_BUCKET_PREVIEWS,
+  templates: process.env.STORAGE_BUCKET_TEMPLATES,
+};
+
 const s3Client = new S3Client({
   region: "auto",
-  endpoint: process.env.R2_ENDPOINT,
+  endpoint: process.env.STORAGE_ENDPOINT,
   credentials: {
-    accessKeyId: process.env.R2_ACCESS_KEY_ID,
-    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.STORAGE_ACCESS_KEY_ID,
+    secretAccessKey: process.env.STORAGE_SECRET_ACCESS_KEY,
   },
 });
 
-export default s3Client;
+export { BUCKETS, s3Client };
