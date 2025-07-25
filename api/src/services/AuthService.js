f59d4e41380b908aa6d36dc5c8d427f8e92e30be
Pooria Soltani <pooria.ms@gmail.com>
2025-07-25 13:13:39 +0100

    ✨ feat: fix user role assignment in user creation process

--- api/src/services/AuthService.js
+++ api/src/services/AuthService.js
@@ -20,6 +20,7 @@ export default class AuthService {
       email,
       passwordHash,
       confirmationToken,
+      "user",
       publicId
     );
     await mailer.sendConfirmationEmail(email, confirmationToken);
